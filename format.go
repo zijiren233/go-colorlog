d264b7ad6ed109ddbb65e664ce56a02aa923cfdc
zijiren233 <pyh1670605849@gmail.com>
2023-05-11 20:29:32 +0800

    Fix: time format

--- format.go
+++ format.go
@@ -14,17 +14,21 @@ func (l *logger) writeToFile(msgtmp *logmsg, file io.Writer) (n int, err error) 
 		return 0, errFileNil
 	}
 	if l.levle == L_Debug || msgtmp.levle >= L_Error {
-		return fmt.Fprintf(file, "%s [%s] [%s|%s|%d] %s\n", msgtmp.time, IntToLevle(msgtmp.levle), msgtmp.filename, msgtmp.funcName, msgtmp.line, msgtmp.message)
+		return fmt.Fprintf(file, "%s [%s] [%s|%s|%d] %s\n", msgtmp.time.Format(timeFormate), IntToLevle(msgtmp.levle), msgtmp.filename, msgtmp.funcName, msgtmp.line, msgtmp.message)
 	}
-	return fmt.Fprintf(file, "%s [%s] %s\n", msgtmp.time, IntToLevle(msgtmp.levle), msgtmp.message)
+	return fmt.Fprintf(file, "%s [%s] %s\n", msgtmp.time.Format(timeFormate), IntToLevle(msgtmp.levle), msgtmp.message)
 }
 
 func (l *logger) logprint(msgtmp *logmsg) {
 	if l.logPrint {
-		fmt.Fprintf(l.stdout, "%s |%s %s %s| %s\n", msgtmp.time, levleColor(msgtmp.levle), IntToLevle(msgtmp.levle), reset, msgtmp.message)
+		fmt.Fprintf(l.stdout, "%s |%s %s %s| %s\n", msgtmp.time.Format(timeFormate), levleColor(msgtmp.levle), IntToLevle(msgtmp.levle), reset, msgtmp.message)
 	}
 }
 
 func Format(time time.Time, levle LogLevle, message string) string {
 	return fmt.Sprintf("%s |%s %s %s| %s", time.Format(timeFormate), levleColor(levle), IntToLevle(levle), reset, message)
+}
+
+func Formatln(time time.Time, levle LogLevle, message string) string {
+	return fmt.Sprintf("%s |%s %s %s| %s\n", time.Format(timeFormate), levleColor(levle), IntToLevle(levle), reset, message)
 }
